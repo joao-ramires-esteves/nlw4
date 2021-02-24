1b4f31f63f94f54972a6d3e8fd738aa476241dae
joao-ramires-esteves <pedromaluco2001@gmail.com>
2021-02-24 15:52:20 -0300

    implementado msgs de erro significativas

--- rocketpay/lib/rocketpay_web/views/error_view.ex
+++ rocketpay/lib/rocketpay_web/views/error_view.ex
@@ -1,6 +1,10 @@
 defmodule RocketpayWeb.ErrorView do
   use RocketpayWeb, :view
 
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
+  alias Ecto.Changeset
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
@@ -12,5 +16,19 @@ defmodule RocketpayWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  # so da match nessaq se result for um changeset (aka se deu erro)
+  def render("400.json", %{ result: %Changeset{} = var_changeset }) do
+    %{message: translate_errors(var_changeset)}
+  end
+
+  # tirado diretamente do exemplo da doc
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
